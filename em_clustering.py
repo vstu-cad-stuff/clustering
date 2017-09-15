463ca18ecb8835af9d8d02a8887e33036ec9973a
_illaech <illaech@gmail.com>
2017-09-15 21:53:53 +0300

    upd

--- em_clustering.py
+++ em_clustering.py
@@ -1,15 +1,17 @@
 from __future__ import division, print_function
 from math import ceil, floor
-import random as rnd
 import os
-import numpy as np
 import json
 import time
+import numpy as np
+import random as rnd
 from geographiclib.geodesic import Geodesic
 from multiprocessing.dummy import Pool as ThreadPool
 from argparse import ArgumentParser
 from geodata import Geodata as Gd, Point as Pt, Cluster as Cl
 from routelib import Route
+
+# special symbol: ___
 
 parser = ArgumentParser(description='EM clustering.')
 parser.add_argument('-i', '--iteration', type=int, help='Iterations number.', default=100)
@@ -20,11 +22,24 @@ parser.add_argument('-p', '--points', nargs='+', help='Load points from file whi
                           #required=True)
 parser.add_argument('-r', '--random', type=int, nargs=1, help='Initialize clusters by uniform distribution of points. Argument is the number of clusters.')
 parser.add_argument('-g', '--grid', type=int, nargs=2, help='Initialize clusters by grid. Arguments are numbers of columns and rows of grid.')
-parser.add_argument('--threads', type=int, nargs=1, help='', default=4)
+parser.add_argument('--threads', type=int, nargs=1, help='Number of paralel threads ___working together for better future___', default=4)
+
+args = parser.parse_args()
+
+cell_size = 50 # cell size in meters
+
+threads = args.threads
+pool = ThreadPool(processes=threads)
+
+route = Route()
+
+path = str(time.time()).replace('.', '_') # ___use datetime and filename___
+fit_time = 0
 
 # ============== data files ==============
 
 def load_file(filename, params=[]):
+    # load data from file
     with open(filename) as file_:
         arr = json.loads(file_.readlines()[0])
     data = [Pt(float(i['lat']), float(i['lon']), 0) for i in arr if i['type'] not in params]
@@ -32,6 +47,7 @@ def load_file(filename, params=[]):
     return data
 
 def export_file(filename, data):
+    # save data to file
     fpath = os.path.dirname(filename)
     if not (os.path.exists(fpath)):
         os.makedirs(fpath)
@@ -50,10 +66,10 @@ def get_bounds(points):
     # choose four coordinates
     # of different points as bounds:
     # most bottom, most left, most top, and most right
-    b = min(map(lambda x: x.lat, p))
-    l = min(map(lambda x: x.lon, p))
-    t = max(map(lambda x: x.lat, p))
-    r = max(map(lambda x: x.lon, p))
+    b = min(map(lambda x: x.lat, points))
+    l = min(map(lambda x: x.lon, points))
+    t = max(map(lambda x: x.lat, points))
+    r = max(map(lambda x: x.lon, points))
     bounds = [b, l, t, r]
     return bounds
 
@@ -101,15 +117,6 @@ def init_file(filename, params=[]):
 
 # ============== global ==============
 
-args = parser.parse_args()
-
-threads = args.threads
-pool = ThreadPool(processes=threads)
-
-route = Route()
-
-path = str(time.time()).replace('.', '_')
-
 def asyncWorker(iterator, func, data=None):
     thread_list = []
     result = []
@@ -156,8 +163,19 @@ def inside(p, h):
     
 def inline_print(digits, text):
     delete = '\r' * digits
-    print('{}{}'.format(delete, text), end='')
+    print('{}{}'.format(delete, text), end='') # ___print special sympbol that i don't remember at the moment___
     return len(text)
+
+def time_print(value):
+    if value > 86400:
+        text = '{:.4f} days'.format(value / 86400)
+    elif value > 3600:
+        text = '{:.3f} hours'.format(value / 3600)
+    elif value > 60:
+        text = '{:.2f} minutes'.format(value / 60)
+    else:
+        text = '{:.2f} seconds'.format(value)
+    return text
 
 def calculate_table(X):
     lx = len(X)
@@ -206,7 +224,7 @@ def fit(X, C, I):
         return r
 
     def stop(iter, old, new):
-        if iter >= I:
+        if iter > I:
             return True
         return np.array_equal(old, new)
         
@@ -219,40 +237,41 @@ def fit(X, C, I):
     
     lx = len(X)
     lc = len(C)
-    iter = 0
+    iteration = 1
 
     print('Searching for bounds of points set...')
     bnds = getBounds(X)
     # size = max(self.geodist(left-top, right-top), self.geodist(right-bottom, right-top))
-    size_x, size_y = self.geodist(bnds[2:0:-1], bnds[2:5]), self.geodist(bnds[::3], bnds[2:5])
-    size_x, size_y = map(lambda x: int(ceil(x / 50)), (size_x, size_y))
+    size_x, size_y = geodist(bnds[2:0:-1], bnds[2:5]), geodist(bnds[::3], bnds[2:5])
+    size_x, size_y = map(lambda x: int(ceil(x / cell_size)), (size_x, size_y))
     print('  Done. Size of grid: {}x{}'.format(size_x, size_y))
+
     print('Making grid...')
     grid = make_grid([size_x, size_y], getBounds(X), 5)
     print('  Done. Number of elements: {}'.format(len(grid)))
+
     print('Finding unnecessary elements...')
     outside = []
     hull = graham_scan(X)
-    for i, j in enumerate(grid):
-        if not inside(j, hull):
-            outside.append(i)
+    for idx, el in enumerate(grid):
+        if not inside(el, hull):
+            outside.append(idx)
     grid = np.delete(grid, outside, axis=0)
     print('  Done. Number of elements was reduced to {}'.format(len(grid)))
-    print('Removing duplicates...')
+
+    print('Removing possible duplicates...')
     grid = np.array(list(map(lambda x: Pt(x[0], x[1], 0), {tuple(row) for row in grid}))) # this deletes dublicates
     print('  Done. Final number of elements: {}'.format(len(grid)))
+
     table = np.append(X, grid, axis=0)
     np.savetxt('{}/table.txt'.format(path), table, fmt='%s')
     print('Now {} points will be clustering to {} clusters using additional {} elements'.format(lx, lc, len(grid)))
 
-    self.L = np.empty([lx])
-    self.P = None
-    self.A = None
-
     print('Calculating metric...')
     dist_table = calculate_table(table)
     print('  Done.')
-    print('Tabulating metric...')
+
+    print('Fixing distances...')
     fixed = 0
     def yz_loop(x):
         for z in range(x + 1, lx):
@@ -272,47 +291,45 @@ def fit(X, C, I):
 
     # replace all points with their place in table
     X_ = list(range(len(X)))
+
     # finding closest points to cluster centers
-    i = list(map(C.cid))
-    C = np.array(list(map(lambda x: list(closer(x), table), C[:, :2])))
-    self.C = np.append(C, np.reshape(i, (i.shape[0], 1)), axis=1)
+    # algo: find closest point in table, cut it to Geodata to remove cid,
+    # expand back to Point with correct cid, transform to Cluster with 0 pop
+    # and finally make an np.array of clusters
+    C = np.array(list(map(lambda c: Cl.point(Pt.geodata(closer(c, table).to_geodata(), c.cid), 0), C)))
+
+    C_ = None
+
     # while clustering isn't completed
-    while not self.stop(iteration, c_old, self.C, l_old, self.L):
+    while not self.stop(iteration, C_, C):
         time_start = time.time()
         # reset population in clusters
-        self.P = np.zeros([lc])
+        for c in C:
+            c.pop = 0
+
         # show iteration number
-        print('Iteration {}:'.format(iteration + 1))
+        print('Iteration {}:'.format(iteration))
+
         # create empty python array
         # each item will contain all the points belongs to specific cluster
-        self.A = [np.empty(0, dtype='object') for i in range(lc)]
+        A = np.empty(lc, dtype='object')
+
         # for each point
         print('  assigning points...')
-        l_old = np.array(self.L)
-        res = asyncWorker(range(lx), self.xloop)
         for i in range(lx):
             # calculate distance between point and all the clusters centers
-            distances = [(tab_dist(X[i], C[each]), each) for each in range(lc)]
+            distances = [(tab_dist(X[i], C[j]), j) for j in range(lc)]
             # sort distances ascending
             distances.sort(key=lambda x: x[0])
             # pick number of cluster, which center has the smallest
             # distance to point
-            m = distances[0][1]
+            k = distances[0][1]
             # set label of point
-            L[i] = C[m][2]
-            P[m] += 1
-            arrays[m] = np.append(arrays[m], [X[i]], axis=0)
+            X[i] = C[k].cid
+            C[k].pop += 1
+            A[k] = np.append(A[k], X[i])
         # equate the previous and current centers of clusters
-        c_old = self.C
-
-        cc = self.C
-        cc = list(map(lambda x, y: (np.append(x, y)).tolist(), cc, self.P))
-        filename = '{}/r_centers_{}.js'.format(path, iteration + 1)
-        dump(cc, filename)
-        xc = X
-        xc = list(map(lambda x, y: (np.append(list(x), y)).tolist(), xc, self.L))
-        filename = '{}/r_points_{}.js'.format(path, iteration + 1)
-        dump(xc, filename)
+        C_ = C
 
         # array for calculated centers of clusters
         mu = np.empty([lc, 3], dtype='object')
@@ -321,36 +338,28 @@ def fit(X, C, I):
         # for each cluster
         i = 0
         while i < lc:
-            # if it contains points
-            if self.P[i] != 0:
-                # calculate center of cluster
-                k = np.array(np.round(np.mean(list(map(list, self.A[i])), axis=0).astype(np.double), decimals=6), dtype='object')
-                mu[i] = np.append(k, i)
+            if C[i].pop != 0:
+                # if it contains points, calculate new center of cluster
+                c = np.array(np.round(np.mean(list(map(list, A[i].latlon())), axis=0).astype(np.double), decimals=5), dtype='object')
             else:
-                d = np.round(self.C[i][:2].astype(np.double), decimals=6)
-                mu[i] = np.append(d.astype(np.object), i)
+                # otherwise: just leave it with old coordinates
+                c = np.round(C[i].latlon().astype(np.double), decimals=5)
+            mu[i] = np.append(c, i)
             i += 1
+
         print('  replacing old centers with new...')
-        # equate current centroids to calculated
-        i = mu[:, 2]
-        mu = np.array(list(map(lambda x: list(self.closer(x)), mu[:, :2])))
-        self.C = np.append(mu, np.reshape(i, (i.shape[0], 1)), axis=1)
+        mu = np.array(list(map(lambda i, c: Cl.point(Pt.geodata(closer(Gd(c), table).to_geodata(), C[i].cid), C[i].pop), enumerate(mu))))
+        C = mu
         # increment iteration counter
         iteration += 1
         iter_time = time.time() - time_start
-        if iter_time > 86400:
-            iter_time = '{:.4f} days'.format(iter_time / 86400)
-        elif iter_time > 3600:
-            iter_time = '{:.4f} hours'.format(iter_time / 3600)
-        elif iter_time > 60:
-            iter_time = '{:.4f} minutes'.format(iter_time / 60)
-        else:
-            iter_time = '{:.4f} seconds'.format(iter_time)
-        print('  iteration ended in {}\n'.format(iter_time))
-    # record results
-    self.clusterCenters = self.C
-    self.labels = self.L
-    self.population = self.P
+        fit_time += iter_time
+        print('  iteration ended in {}\n'.format(time_print(iter_time)))
+
+        print('  saving results...')
+        export_file('{}/r_centers_{}.js'.format(path, iteration), C)
+        export_file('{}/r_points_{}.js'.format(path, iteration), X)
+    # end while
 
 # ============== main ==============
 
@@ -374,9 +383,5 @@ if __name__ == '__main__':
         os.makedirs(path)
         
     result = fit(X, clusters, max_iter)
-    print('Fit time: {}, clusters: {}. Saved in {}'.format(result['fitTime'], result['numCluster'], path))
-
-    # export centers to 'centers.js'
-    export_file('{}/cls.js'.format(path), clusters.tolist())
-    # export points to 'points.js'
-    export_file('{}/pts.js'.format(path), X.tolist())
+    print('Fit time: {}. Saved in {}'.format(fit_time, path))
+    
